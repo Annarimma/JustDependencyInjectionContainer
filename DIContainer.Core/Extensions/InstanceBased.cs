8694e1baf6fc9bd3448d1bb28df8db9d70714fdd
Annarimma <lida32152@gmail.com>
2023-02-19 16:52:02 -0800

    added instance based extensions

--- DIContainer.Core/Extensions/InstanceBased.cs
+++ DIContainer.Core/Extensions/InstanceBased.cs
@@ -1,3 +1,7 @@
+using System;
+using DIContainer.Core.Abstraction;
+using DIContainer.Core.MetaInfo;
+
 namespace DIContainer.Core.Extensions;
 
 /// <summary>
@@ -5,5 +9,46 @@ namespace DIContainer.Core.Extensions;
 /// </summary>
 public static class InstanceBased
 {
-    // todo extensions
+    /// <summary>
+    /// Singleton registration
+    /// </summary>
+    /// <param name="builder">Container builder</param>
+    /// <param name="interface">Type of Interface</param>
+    /// <param name="instance">Instance</param>
+    /// <returns>IContainerBuilder</returns>
+    public static IContainerBuilder AddSingleton(this IContainerBuilder builder,
+        Type @interface,
+        object instance)
+    {
+        return builder.RegisterInstance(@interface, instance);
+    }
+    
+    /// <summary>
+    /// Singleton registration
+    /// </summary>
+    /// <param name="builder">Container builder</param>
+    /// <param name="instance">Instance</param>
+    /// <typeparam name="TInterface">Type of Interface</typeparam>
+    /// <returns>IContainerBuilder</returns>
+    public static IContainerBuilder AddSingleton<TInterface>(this IContainerBuilder builder,
+        object instance)
+    {
+        return builder.RegisterInstance(typeof(TInterface), instance);
+    }
+
+    /// <summary>
+    /// General method for Instance Registration
+    /// </summary>
+    /// <param name="builder">Container builder</param>
+    /// <param name="interface">Type of Interface</param>
+    /// <param name="instance">Instance</param>
+    /// <returns>IContainerBuilder</returns>
+    private static IContainerBuilder RegisterInstance(this IContainerBuilder builder, 
+        Type @interface, 
+        object instance)
+    {
+        builder
+            .Register(new InstanceBasedServiceDescriptor(@interface, instance));
+        return builder;
+    }
 }
